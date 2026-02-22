355ae860fbd9ad26312f91e36ba83ec0f71bd9a6
LuisAcevedoLp <luisacevedo2003@outlook.com>
2026-02-22 03:26:18 -0600

    Creacion de archivos de inico para front y back

---
+++ backend/src/main/java/com/LAAG/madlibs/MadlibsGameApplication.java
@@ -0,0 +1,13 @@
+package com.LAAG.madlibs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadlibsGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadlibsGameApplication.class, args);
+	}
+
+}
